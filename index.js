3129a1a9f0c6eb6b1d828b90b184ee94aaf52943
MartinWakaba <martin.wakaba@student.moringaschool.com>
2023-09-29 15:45:15 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+let new_peep = [];
+let peep = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(peep_name, event_name){
+    for(let count = 0; count < peep_name.length; count++){
+        new_peep.push(`Thank you, ${peep_name[count]}, for the wonderful ${event_name} gift!`);
+    }
+    return new_peep;
+
+}
+
+function countDown(num){
+    while(num >= 0){
+        console.log(num);
+        num--;
+    }
+}
+countDown(10);
+writeCards(peep, "surprise");
